1d2d4726f6bbc23c6f398fee191af7b0df0e4263
Hassan Shakil <h>
2020-12-11 14:50:03 +0500

    initial commit

---
+++ H4X0R/Views/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  H4X0R
+//
+//  Created by MAC on 07/12/2020.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(
+                    destination: DetailView(url: post.url),
+                    label: {
+                        HStack {
+                            Text(String(post.points))
+                            Text(post.title)
+                        }
+                    })
+            }
+            .navigationBarTitle("H4X0R NEWS")
+        }
+        .onAppear(perform: {
+            self.networkManager.fetchData()
+        })
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4X0R/Views/DetailView.swift
@@ -0,0 +1,27 @@
+//
+//  DetailView.swift
+//  H4X0R
+//
+//  Created by MAC on 07/12/2020.
+//
+
+import SwiftUI
+import WebKit
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+        
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
+
+
